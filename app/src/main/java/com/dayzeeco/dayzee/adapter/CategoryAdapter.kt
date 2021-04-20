657a64fac2eeef7fc776deabfb4f20ffd4ea642c
Samuel Zaffran <zaffransamuel@gmail.com>
2021-04-20 13:39:31 +0300

    1.709

--- app/src/main/java/com/dayzeeco/dayzee/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/dayzeeco/dayzee/adapter/CategoryAdapter.kt
@@ -9,6 +9,7 @@ import com.dayzeeco.dayzee.R
 import com.dayzeeco.dayzee.model.Category
 import kotlinx.android.synthetic.main.adapter_suggestion_card.view.*
 import kotlinx.android.synthetic.main.item_category.view.*
+import java.util.*
 
 class CategoryAdapter(private val categories: List<Category>?, private val categoriesAlreadyChosen : List<Category>?) : BaseAdapter() {
 
@@ -27,26 +28,38 @@ class CategoryAdapter(private val categories: List<Category>?, private val categ
         }
 
         view.category_tv.text = category?.category
-        when(category?.category){
-            view.context.getString(R.string.sports), view.context.getString(R.string.sport) -> view.category_iv.setImageDrawable(
+        when(category?.category?.toLowerCase()){
+            view.context.getString(R.string.sports).toLowerCase(), view.context.getString(R.string.sport).toLowerCase() -> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_sport))
-            view.context.getString(R.string.esports), view.context.getString(R.string.esport) -> view.category_iv.setImageDrawable(
+            view.context.getString(R.string.crypto).toLowerCase() -> view.category_iv.setImageDrawable(
+                ContextCompat.getDrawable(view.context, R.drawable.category_crypto))
+            view.context.getString(R.string.influencers).toLowerCase() -> view.category_iv.setImageDrawable(
+                ContextCompat.getDrawable(view.context, R.drawable.category_influencers))
+            view.context.getString(R.string.social_and_meeting).toLowerCase() -> view.category_iv.setImageDrawable(
+                ContextCompat.getDrawable(view.context, R.drawable.category_social_and_meeting))
+            view.context.getString(R.string.for_kids).toLowerCase() -> view.category_iv.setImageDrawable(
+                ContextCompat.getDrawable(view.context, R.drawable.category_for_kids))
+            view.context.getString(R.string.esports).toLowerCase(), view.context.getString(R.string.esport).toLowerCase() -> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_esport))
-            view.context.getString(R.string.holidays), view.context.getString(R.string.holiday) -> view.category_iv.setImageDrawable(
+            view.context.getString(R.string.music).toLowerCase() -> view.category_iv.setImageDrawable(
+                ContextCompat.getDrawable(view.context, R.drawable.category_music))
+            view.context.getString(R.string.holidays).toLowerCase(), view.context.getString(R.string.holiday).toLowerCase() -> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_holidays))
-            view.context.getString(R.string.shopping) -> view.category_iv.setImageDrawable(
+            view.context.getString(R.string.shopping).toLowerCase() -> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_shopping))
-            view.context.getString(R.string.films_n_amp_series) -> view.category_iv.setImageDrawable(
+            view.context.getString(R.string.films_n_amp_series).replace("\n", "").toLowerCase(),
+            view.context.getString(R.string.behind_the_screen).toLowerCase()-> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_film))
-            view.context.getString(R.string.culture_n_amp_loisirs) -> view.category_iv.setImageDrawable(
+            view.context.getString(R.string.culture_n_amp_loisirs).replace("\n", "").toLowerCase(),
+            view.context.getString(R.string.culture).toLowerCase()-> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_culture))
-            view.context.getString(R.string.youtubers) -> view.category_iv.setImageDrawable(
+            view.context.getString(R.string.youtubers), view.context.getString(R.string.youtube_channels).toLowerCase() -> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_youtuber))
-            view.context.getString(R.string.religions) -> view.category_iv.setImageDrawable(
+            view.context.getString(R.string.religions).toLowerCase(), view.context.getString(R.string.religion).toLowerCase() -> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_religion))
-            view.context.getString(R.string.calendrier_n_interessant) -> view.category_iv.setImageDrawable(
+            view.context.getString(R.string.calendrier_n_interessant).replace("\n", "").toLowerCase() -> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_interesting_calendar))
-            view.context.getString(R.string.fair_trade) -> view.category_iv.setImageDrawable(
+            view.context.getString(R.string.fair_trade), view.context.getString(R.string.events_and_fair_trade).toLowerCase() -> view.category_iv.setImageDrawable(
                 ContextCompat.getDrawable(view.context, R.drawable.category_else))
             else -> view.category_iv.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.audiance))
         }
