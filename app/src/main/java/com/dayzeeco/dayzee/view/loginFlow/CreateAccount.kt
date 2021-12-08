f8bba267dce6ce8f736c6727a7c78722621e00a5
Samuel Zaffran <zaffransamuel@gmail.com>
2021-12-08 18:29:44 +0200

    2.34

--- app/src/main/java/com/dayzeeco/dayzee/view/loginFlow/CreateAccount.kt
+++ app/src/main/java/com/dayzeeco/dayzee/view/loginFlow/CreateAccount.kt
@@ -1,14 +1,17 @@
 package com.dayzeeco.dayzee.view.loginFlow
 
+import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
-import android.text.Editable
-import android.text.TextUtils
-import android.text.TextWatcher
+import android.text.*
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -23,6 +26,7 @@ import com.dayzeeco.dayzee.viewModel.MeViewModel
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_create_account.*
 import java.util.*
+
 
 class CreateAccount : Fragment(), View.OnClickListener {
 
@@ -49,6 +53,11 @@ class CreateAccount : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         prefs = PreferenceManager.getDefaultSharedPreferences(context)
+
+        agreee_tv.makeLinks(
+            Pair(resources.getString(R.string.terms), View.OnClickListener {  }),
+            Pair(resources.getString(R.string.privacy_policy), View.OnClickListener {  })
+        )
 
         handlerMail = Handler { msg ->
             if (msg.what == TRIGGER_AUTO_COMPLETE) {
@@ -101,21 +110,27 @@ class CreateAccount : Fragment(), View.OnClickListener {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 handlerPasswordSignup.removeMessages(TRIGGER_AUTO_COMPLETE)
-                handlerPasswordSignup.sendEmptyMessageDelayed(TRIGGER_AUTO_COMPLETE, AUTO_COMPLETE_DELAY)
+                handlerPasswordSignup.sendEmptyMessageDelayed(
+                    TRIGGER_AUTO_COMPLETE,
+                    AUTO_COMPLETE_DELAY
+                )
             }
 
         })
-        signup_username.addTextChangedListener(object: TextWatcher{
+        signup_username.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 handlerIdentifiant.removeMessages(TRIGGER_AUTO_COMPLETE)
-                handlerIdentifiant.sendEmptyMessageDelayed(TRIGGER_AUTO_COMPLETE, AUTO_COMPLETE_DELAY)
+                handlerIdentifiant.sendEmptyMessageDelayed(
+                    TRIGGER_AUTO_COMPLETE,
+                    AUTO_COMPLETE_DELAY
+                )
             }
 
         })
-        signup_mail.addTextChangedListener(object: TextWatcher{
+        signup_mail.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -133,47 +148,98 @@ class CreateAccount : Fragment(), View.OnClickListener {
         when(v) {
             signup_connect -> {
 
-                if(availableIdentifiant != null && availableMail != null){
-                    if(availableIdentifiant!! && availableMail!! && passwordValidForm){
-                        loginViewModel.checkAddUser(UserSignUpBody(signup_mail.text.toString(), signup_username.text.toString(), signup_password.text.toString())).observe(viewLifecycleOwner,
+                if (availableIdentifiant != null && availableMail != null) {
+                    if (availableIdentifiant!! && availableMail!! && passwordValidForm) {
+                        loginViewModel.checkAddUser(
+                            UserSignUpBody(
+                                signup_mail.text.toString(),
+                                signup_username.text.toString(),
+                                signup_password.text.toString()
+                            )
+                        ).observe(viewLifecycleOwner,
                             {
-                                when(it.code()){
+                                when (it.code()) {
                                     201 -> {
-                                        meViewModel.modifyProfile(it.body()?.token!!, UpdateUserInfoDTO(language = Locale.getDefault().language, status = 0, dateFormat = 0, socialMedias = SocialMedias(
-                                            Youtube("", false), Facebook("", false), Instagram("", false), WhatsApp("", false), LinkedIn("", false),
-                                            Twitter("", false), Discord("", false), Telegram("", false)
-                                        )
-                                        )
+                                        meViewModel.modifyProfile(
+                                            it.body()?.token!!, UpdateUserInfoDTO(
+                                                language = Locale.getDefault().language,
+                                                status = 0,
+                                                dateFormat = 0,
+                                                socialMedias = SocialMedias(
+                                                    Youtube("", false),
+                                                    Facebook("", false),
+                                                    Instagram(
+                                                        "",
+                                                        false
+                                                    ),
+                                                    WhatsApp("", false),
+                                                    LinkedIn("", false),
+                                                    Twitter("", false),
+                                                    Discord("", false),
+                                                    Telegram(
+                                                        "",
+                                                        false
+                                                    )
+                                                )
+                                            )
                                         ).observe(viewLifecycleOwner, { rp ->
-                                            if(rp.isSuccessful){
-                                                findNavController().navigate(CreateAccountDirections.actionCreateAccountToPreferenceCategory(true))
-                                                prefs.edit().putString(accessToken, it.body()?.token).apply()
-                                                prefs.edit().putString(refreshToken, it.body()?.refreshToken).apply()
-                                                prefs.edit().putString(user_info_dto, Gson().toJson(it.body()?.user)).apply()
-                                                prefs.edit().putInt(followers, it.body()?.user?.followers!!).apply()
-                                                prefs.edit().putInt(following, it.body()?.user?.following!!).apply()
+                                            if (rp.isSuccessful) {
+                                                findNavController().navigate(
+                                                    CreateAccountDirections.actionCreateAccountToPreferenceCategory(
+                                                        true
+                                                    )
+                                                )
+                                                prefs.edit().putString(
+                                                    accessToken,
+                                                    it.body()?.token
+                                                ).apply()
+                                                prefs.edit().putString(
+                                                    refreshToken,
+                                                    it.body()?.refreshToken
+                                                ).apply()
+                                                prefs.edit().putString(
+                                                    user_info_dto, Gson().toJson(
+                                                        it.body()?.user
+                                                    )
+                                                ).apply()
+                                                prefs.edit().putInt(
+                                                    followers,
+                                                    it.body()?.user?.followers!!
+                                                ).apply()
+                                                prefs.edit().putInt(
+                                                    following,
+                                                    it.body()?.user?.following!!
+                                                ).apply()
                                             }
                                         })
 
                                     }
                                     409 -> {
-                                        Toast.makeText(requireContext(), getString(R.string.invalid_authentication), Toast.LENGTH_SHORT).show()
+                                        Toast.makeText(
+                                            requireContext(),
+                                            getString(R.string.invalid_authentication),
+                                            Toast.LENGTH_SHORT
+                                        ).show()
                                     }
                                     400 -> {
-                                        Toast.makeText(requireContext(), getString(R.string.invalid_authentication), Toast.LENGTH_SHORT).show()
+                                        Toast.makeText(
+                                            requireContext(),
+                                            getString(R.string.invalid_authentication),
+                                            Toast.LENGTH_SHORT
+                                        ).show()
                                     }
                                 }
                             })
                     }
                 }
 
-                if(signup_mail.text.isNullOrBlank()){
+                if (signup_mail.text.isNullOrBlank()) {
                     signup_mail.error = getString(R.string.field_cannot_be_empty)
                 }
-                if(signup_username.text.isNullOrBlank()){
+                if (signup_username.text.isNullOrBlank()) {
                     signup_username.error = getString(R.string.field_cannot_be_empty)
                 }
-                if(signup_password.text.isNullOrBlank()){
+                if (signup_password.text.isNullOrBlank()) {
                     signup_password.error = getString(R.string.field_cannot_be_empty)
                 }
 
@@ -181,4 +247,31 @@ class CreateAccount : Fragment(), View.OnClickListener {
         }
     }
 
+}
+
+fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>) {
+    val spannableString = SpannableString(this.text)
+    var startIndexOfLink = -1
+    for (link in links) {
+        val clickableSpan = object : ClickableSpan() {
+            override fun updateDrawState(textPaint: TextPaint) {
+                textPaint.color = resources.getColor(R.color.colorText)
+                textPaint.isUnderlineText = false
+            }
+
+            override fun onClick(view: View) {
+                Selection.setSelection((view as TextView).text as Spannable, 0)
+                view.invalidate()
+                link.second.onClick(view)
+            }
+        }
+        startIndexOfLink = this.text.toString().indexOf(link.first, startIndexOfLink + 1)
+        spannableString.setSpan(
+            clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+    }
+    this.movementMethod =
+        LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
+    this.setText(spannableString, TextView.BufferType.SPANNABLE)
 }
